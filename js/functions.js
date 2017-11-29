d8822ea510b8a9daba2b80d05581dd6075f449cf
AJ-Ora <antti-jussioravainen@kamk.fi>
2017-11-29 17:23:47 +0200

    Added more memes.

--- js/functions.js
+++ js/functions.js
@@ -5,3 +5,21 @@ function GetTime() {
 function HelloWorld() {
 	document.getElementById("demo").innerHTML = "Hello world!";
 }
+
+function GetRandomNumber() {
+	document.getElementById("demo").innerHTML = Math.random();
+}
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
